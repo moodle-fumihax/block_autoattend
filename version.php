77e55a4a63781b9ba6b319dc7e515bea42cbee0e
root <iseki@rsch.tuis.ac.jp>
2025-02-25 11:10:57 +0900

    v2.7.5 fix default locale at jbxl_strftime

--- version.php
+++ version.php
@@ -9,9 +9,10 @@ $plugin->cron      = 0;
 $plugin->maturity  = MATURITY_STABLE;
 
 //$plugin->release = '2.7.4';
-$PLUGIN_release    = '2.7.4';
+$PLUGIN_release    = '2.7.5';
 
-$plugin->version   = 2025020300;    // v2.7.4 strftime(deprecated) -> jbxl_strftime and $plugin->release -> $PLUGIN_release
+$plugin->version   = 2025022500;    // v2.7.5 fix default locale at jbxl_strftime
+//$plugin->version = 2025020300;    // v2.7.4 strftime(deprecated) -> jbxl_strftime and $plugin->release -> $PLUGIN_release
 //$plugin->version = 2024032301;    // v2.7.3 get_string('grade') -> get_string('attendgrade', 'block_autoattend');
 //$plugin->version = 2024021103;    // v2.7.2 repackege
 //$plugin->version = 2024011102;    // v2.7.1 contorol show attendance info. in the block
